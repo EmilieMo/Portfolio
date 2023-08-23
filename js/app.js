068b5d811770b0c1581074bd8053f4cf7fcb04f5
EmilieMo <mimimilie120@hotmail.com>
2023-08-23 17:37:37 +0200

    Third commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+console.log('toto');
+
+// ___________string____________________ =chaine de caractères donc du texte
+        let myVar = 'ma variable';
+        // on défini la variable = on met sa valeur
+        myVar = 'ma variable changé'
+        // on peut créer ou changer une variable en la réinitialiser
+        const myVar2 = "ma variable 2";
+        //  le const est une variable qui ne change pas
+        // par ex: myVar2 = "ma variable 2 changée"; indiquera une rreur car on ne peut pas changer la const
+console.log(myVar);
+// affiche la variable changé qui a été réinitialiser
+
+
+// ______________boolean__________________________ = vrai ou faux
+        let isTrue = true;
+        let isFalse = false;
+console.log(isTrue);
+//  si c'est vrai alors on va pourvoir faire quelque chose
+
+// _____________chiffres et opérateurs_______________
+        let chiffre1 = 4; // pas besoin de mettre les ' guillemets ' car c'est un chiffres sinon c'est considéré comme une chaine de caractère
+        let chiffre2 = 3; 
+console.log(chiffre1, chiffre2); // nous donne les 2 chiffres = 4 et 3
+console.log(chiffre1 + chiffre2); // additionne 4 + 3 DONC 7
+console.log(chiffre1 - chiffre2); // soustrait 4 + 3 DONC 1
+console.log(chiffre1 / chiffre2); // divise 4 / 3 DONC 1.333333333333
+console.log(chiffre1 * chiffre2); // multiplie 4 * 3 DONC 12
+
